807c09d24de75ecb496596a90f8516d4470b879a
Thomas <t.champagne@sesamm.com>
2020-12-11 09:43:51 +0100

    improve test

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -1,22 +1,58 @@
+import { DynamicModule } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { Test, TestingModule } from '@nestjs/testing';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import {
+  ClickedLinkEvent,
+  ClickedLinkEventSchema,
+} from './schemas/ click-link-event.schema';
+import {
+  SignUpEvent,
+  SignUpEventSchema,
+} from './schemas/ sign-up-event.schema';
+import { Event, EventSchema } from './schemas/event.schema';
 
 describe('AppController', () => {
-  let appController: AppController;
+  const testCase: [string, DynamicModule][] = [
+    [
+      'forFeature',
+      MongooseModule.forFeature([
+        {
+          name: Event.name,
+          schema: EventSchema,
+          discriminators: [
+            { name: ClickedLinkEvent.name, schema: ClickedLinkEventSchema },
+            { name: SignUpEvent.name, schema: SignUpEventSchema },
+          ],
+        },
+      ]),
+    ],
+    [
+      'forFeatureAsync',
+      MongooseModule.forFeatureAsync([
+        {
+          name: Event.name,
+          useFactory: async () => EventSchema,
+          discriminators: [
+            { name: ClickedLinkEvent.name, schema: ClickedLinkEventSchema },
+            { name: SignUpEvent.name, schema: SignUpEventSchema },
+          ],
+        },
+      ]),
+    ],
+  ];
 
-  beforeEach(async () => {
-    const app: TestingModule = await Test.createTestingModule({
+  it.each(testCase)('Discriminator - %s', async (_, features) => {
+    const module: TestingModule = await Test.createTestingModule({
+      imports: [MongooseModule.forRoot('mongodb://localhost/nest'), features],
       controllers: [AppController],
       providers: [AppService],
     }).compile();
-
-    appController = app.get<AppController>(AppController);
-  });
-
-  describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
-    });
+    const app = module.createNestApplication();
+    await app.init();
+    const appController = app.get<AppController>(AppController);
+    expect(appController.getHello()).toBe('Hello World!');
+    await app.close();
   });
 });
